d82f21f1e281b54454a656d3e30b8072bdab19a1
AinPhoenix <MuhammadAliIrtaza@gmail.com>
2022-07-02 11:11:49 +0500

    change

--- main.py
+++ main.py
@@ -35,31 +35,36 @@ similarity = cosine_similarity(feature_vector)
 
 # getting the movie name from the user
 
-# movie_name = input("Enter your favourite movie name")
-movie_name = "batman"
-
 # creating the list of all the movies in the dataset
 list_of_all_titles = movies_data['title'].tolist()
 
-#finding the close match for the movie name given by the user
-find_close_match = difflib.get_close_matches(movie_name, list_of_all_titles)
-close_match = find_close_match[0]
+def DisplayMovie(movie):
+    # movie_name = input("Enter your favourite movie name")
+    movie_name = movie
 
-#finding the index of the movie with title
-index_of_the_movie = movies_data[movies_data.title == close_match]['index'].values[0]
+    #finding the close match for the movie name given by the user
+    find_close_match = difflib.get_close_matches(movie_name, list_of_all_titles)
+    close_match = find_close_match[0]
 
-#getting the list of similar movies
-similarity_score = list(enumerate(similarity[index_of_the_movie]))
+    #finding the index of the movie with title
+    index_of_the_movie = movies_data[movies_data.title == close_match]['index'].values[0]
 
-#sorting the movies based on their similarity score
-sorted_similar_movies = sorted(similarity_score, key = lambda x:x[1], reverse = True)
+    #getting the list of similar movies
+    similarity_score = list(enumerate(similarity[index_of_the_movie]))
 
-#print the name of similar movies based on the index
-print("Movies suggested for you:  \n")
-i = 1
-for movie in sorted_similar_movies:
-    index = movie[0]
-    title_from_index = movies_data[movies_data.index == index]['index'].values[0]
-    if i < 30:
-        print(i, ".", list_of_all_titles[title_from_index])
-        i += 1
+    #sorting the movies based on their similarity score
+    sorted_similar_movies = sorted(similarity_score, key = lambda x:x[1], reverse = True)
+
+    #print the name of similar movies based on the index
+    print("Movies suggested for you:  \n")
+    i = 1
+    for movie in sorted_similar_movies:
+        index = movie[0]
+        title_from_index = movies_data[movies_data.index == index]['index'].values[0]
+        if i < 30:
+            print(i, ".", list_of_all_titles[title_from_index])
+            i += 1
+
+while True:
+    name = input("Enter a movie name: ")
+    DisplayMovie(name)
